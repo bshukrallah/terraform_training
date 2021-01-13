d80890d4108105f771f9a80f2f3e3552c8c0a27a
Ben Shukrallah <benjamin.shukrallah@live.longwood.edu>
2021-01-13 13:48:41 -0500

    update IAM

--- iam.tf
+++ iam.tf
@@ -20,5 +20,17 @@ EOF
 
 resource "aws_iam_role" "S3AdminRole" {
     name = "S3AdminRole"
-    assume_role_policy = aws_iam_role_policy.s3AdminPolicy.id
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1610557694422",
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
